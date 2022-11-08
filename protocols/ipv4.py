7161ea275406d3915b4848770411f93309d0f7c7
Ramesh Poudel <rigelstar@Rigelstar.local>
2022-11-08 16:27:29 +0545

    ipv4 protocol related methods and classes

---
+++ protocols/ipv4.py
@@ -0,0 +1,71 @@
+#!/usr/bin/env python3
+
+from enum import Enum
+from transport_layer import TransportLayerPacket
+
+class IPv4:
+    @staticmethod
+    def is_valid_ipv4(ip: str):
+        octets = ip.split('.')
+        for octet in octets:
+            octet_i = int(octet)
+            if octet_i < 0 or octet_i > 255:
+                return False
+        return True
+
+    @staticmethod
+    def is_class_a_ip(ip: str):
+        octets = list(map(int, ip.split('.')))
+        if octets[0] != 10:
+            return False
+        return all(map(lambda x: x >= 0 and x <= 255, [octets[1], octets[2], octets[3]]))
+
+    @staticmethod
+    def is_class_b_ip(ip: str):
+        octets = list(map(int, ip.split('.')))
+        if octets[0] != 172:
+            return False
+
+        o1 = octets[1]
+        o2 = octets[2]
+        o3 = octets[3]
+        return all([(o1 >= 16 and o1 <= 31), (o2 >= 0 and o2 <= 255), (o3 >= 0 and o3 <= 255)])
+
+    @staticmethod
+    def is_class_c_ip(ip: str):
+        octets = list(map(int, ip.split('.')))
+        if (octets[0] != 192) and (octets[2] != 168):
+            return False
+        return all(list(map(lambda x: x >= 0 and x <= 255, [octets[2], octets[3]])))
+
+    @staticmethod
+    def is_private_ip(ip: str):
+        if not IPv4.is_valid_ipv4(ip): return False
+        return any([IPv4.is_class_a_ip(ip), IPv4.is_class_b_ip(ip), IPv4.is_class_c_ip(ip)])
+
+
+class IPv4Packet:
+    NO_FRAG = 0b10
+    MORE_FRAG = 0b1
+
+    class UpperLayerProtocol(Enum):
+        TCP = 6
+        UDP = 17
+
+    def __init__(self, src_ip, dest_ip, upper_layer_protocol, data: TransportLayerPacket, options = None):
+        self.src_ip = src_ip
+        self.dest_ip = dest_ip
+        self.data = data
+        self.upper_layer_protocol = upper_layer_protocol
+        self.header_length = 20 # 20 bytes if there aren't any extra options
+        self.datagram_length =  len(self)
+        self.identifier = 0
+        self.type_of_service = 0
+        self.ttl = 128
+        self.header_checksum = 1111
+        self.fragment_offset = 0
+        self.flags = 0b000
+        self.options = options
+
+    def __len__(self):
+        return self.header_length + len(self.data)

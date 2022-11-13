4015f4f6ffa5b53bd8dd24d99275ba244dbe0a84
Ramesh Poudel <rigelstar@Rigelstar.local>
2022-11-13 14:15:16 +0545

    router's receive method implemnted for ARP request only

--- network_layer.py
+++ network_layer.py
@@ -5,6 +5,7 @@ from enum import Enum
 from ethernet import *
 from transport_layer import *
 from node import Node
+from protocols.arp import ARP
 
 class IPv4Packet:
     NO_FRAG = 0b10
@@ -34,10 +35,15 @@ class IPv4Packet:
 
 
 class Router(Node):
-    def __init__(self, interf_count: int):
-        self.interfaces = {interf_no: EthernetPort() for interf_no in range(interf_count)}
-        self.routing_table = dict()
+    def __init__(self, ip_addr: str, mac_addr: str, interf_count: int):
+        self.interfaces = {interf_no + 1: EthernetPort() for interf_no in range(interf_count)}
+        self.routing_table = None
         self.arp_table = dict()
+        self.ip_addr = ip_addr
+        self.mac_addr = mac_addr
+
+    def configure(self, configs = {"routing_table": None}):
+        self.routing_table = configs.get("routing_table")
 
     def connect(self, device):
         print("Use method 'connect_on_interface': this method is not supported")
@@ -47,7 +53,30 @@ class Router(Node):
         self.interfaces.get(interface_no).connect(device)
 
     def send(self, frame: EthernetFrame):
-        pass
+        return self.send_on_interface(self.routing_table.get("11.11.11.0/24"), frame)
+
+    def send_on_interface(self, inter_no: int, frame: EthernetFrame):
+        return self.interfaces.get(inter_no).connected_device.receive(frame)
 
     def receive(self, frame: EthernetFrame):
-        pass
+        if frame.type == EthernetFrame.ARP:
+            arp = frame.data
+            src_ip = arp.sender_protocol_addr
+            self.arp_table[src_ip] = arp.sender_hardware_addr
+            if arp.type == ARP.REQUEST:
+                if arp.target_protocol_addr == self.ip_addr:
+                    arpp = ARP(
+                            self.mac_addr, 
+                            self.ip_addr, 
+                            arp.sender_hardware_addr, 
+                            arp.sender_protocol_addr, 
+                            typ=ARP.REPLY
+                        )
+                    fram = EthernetFrame(self.mac_addr, frame.src_mac, arpp, EthernetFrame.ARP)
+                    self.send_on_interface(1, fram)
+            elif arp.type == ARP.REPLY:
+                pass
+            else: return False
+        else:
+            ippacket = frame.data
+        return True

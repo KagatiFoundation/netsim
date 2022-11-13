2301da2607aa47c404b68c700a4ef6e3c8a4dbd1
Ramesh Poudel <rigelstar@Rigelstar.local>
2022-11-13 17:01:39 +0545

    calculating length of ICMP protocol

--- protocols/icmp.py
+++ protocols/icmp.py
@@ -1,9 +1,12 @@
 class ICMP:
-    def __init__(self, typ: int, code: int, cksum, data: bytes = None):
+    REQUEST = 8
+    REPLY = 0
+
+    def __init__(self, typ: int, code: int, cksum, data: bytes = b''):
         self.type = typ
         self.code = code
         self.cksum = cksum
         self.data = data
 
     def __len__(self):
-        return 8
+        return 8 + len(self.data)

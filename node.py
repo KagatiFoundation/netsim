d8eb42b483e7395b34ec05f9a4c3b7fe08704f98
rigel-star <boyzdod@gmail.com>
2022-12-09 13:35:02 +0545

    type hinting for Node's methods

--- node.py
+++ node.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
 class Node:
-    def connect(self, device): pass
-    def send(self, frame): pass
-    def receive(self, frame): pass
+    def connect(self, device) -> bool: pass
+    def send(self, frame) -> bool: pass
+    def receive(self, frame) -> bool: pass

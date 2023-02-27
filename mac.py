fd70920e2e279e755aaf67c4766b55d9515e4305
rigel-star <boyzdod@gmail.com>
2023-02-27 21:16:03 +0545

    MACAddr's from_string method improved using regex
    Using regex to check validity of given MAC address string

--- mac.py
+++ mac.py
@@ -1,15 +1,26 @@
 import typing 
 import errors.mac_error as mac_error
+import re
+
+MAC_ADDR_REGEX = re.compile(r'^([a-fA-F0-9]{1,2}:){5}([a-fA-F0-9]{1,2})$')
 
 class MACAddr:
     def __init__(self, octets: typing.List[int]):
         if len(octets) != 6:
-            raise mac_error.MACAddrError(f"MAC address should be 6 bytes long. Given length: '{len(octets)}' bytes.")
+            raise mac_error.MACAddrError(f"MAC address should be 6 bytes long. Given length: '{len(octets)}'.")
 
         for byte in octets:
             if byte < 0 or byte > 255:
                 raise mac_error.MACAddrError(f"'{byte}' is not a valid value for MAC address octet.")
         self.__octets = octets
+
+    @classmethod
+    def from_string(cls, mac: str):
+        matches = re.match(MAC_ADDR_REGEX, mac)
+        if not matches:
+            raise mac_error.MACAddrError(f"Invalid MAC address: '{mac}'.")
+        octets = list(map(lambda x: int(f'0x{x}', 16), mac.split(":")))
+        return cls(octets)
 
     def __getitem__(self, key):
         self.__check_key_validity(key)
@@ -29,5 +40,5 @@ class MACAddr:
         return ":".join([f"{hex(byte)[2:].rjust(2, '0')}" for byte in self.__octets])
 
 if __name__ == "__main__":
-    addr = MACAddr([0xcc, 5, 2, 4, 5, 5])
-    print(addr)
+    mac = MACAddr.from_string("ff:ff:f:3:54:34")
+    print(mac[0])
